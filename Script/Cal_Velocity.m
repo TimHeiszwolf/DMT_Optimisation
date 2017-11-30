cbcb535e44ea01a969ced1acb0101e792e1d3da3
Heiszwolf <t.heiszwolf@student.tue.nl>
2017-11-30 17:11:23 +0100

    ADD Comments to functions
    Added comments to the functions (Cal_Inertia, Cal_Velocity and Cal_kinetic_energy)

--- Script/Cal_Velocity.m
+++ Script/Cal_Velocity.m
@@ -1,3 +1,4 @@
 function Cal_Velocity = func(Energy, Inertia, Length)
+    %Function that calcultes the speed of a point on a rotation arm based on rotational energy https://en.wikipedia.org/wiki/Rotational_energy.
     Cal_Velocity=Length*((2*Energy/Inertia)^0.5)
 end
