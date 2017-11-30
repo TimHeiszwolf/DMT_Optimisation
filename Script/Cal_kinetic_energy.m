314996d26e06632a4a1bf41de772a0fe5f951196
Heiszwolf <t.heiszwolf@student.tue.nl>
2017-11-30 17:12:54 +0100

    FIX Capital letters function name
    Fixed the lack of capital letters in the function name of Cal_Kinetic_Energy.

--- Script/Cal_kinetic_energy.m
+++ Script/Cal_kinetic_energy.m
@@ -1,4 +1,4 @@
-function Cal_kinetic_energy = func(Mass_counter, Length_counter, Mass_projectile, Length_projectile, Density, Start_angle, End_angle)
+function Cal_Kinetic_Energy = func(Mass_counter, Length_counter, Mass_projectile, Length_projectile, Density, Start_angle, End_angle)
     %Calculats the avalible kinetic energy based on the potential gravitational energy: https://en.wikipedia.org/wiki/Gravitational_energy.
     Comb = Length_counter*Mass_counter+0.5*Density*Length_counter^2-0.5*Density*Length_projectile^2-Length_projectile*Mass_projectile
     
