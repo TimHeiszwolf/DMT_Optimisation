e01344f1e40023eaab544c1ae30d9e63bc8d1787
Heiszwolf <t.heiszwolf@student.tue.nl>
2017-11-30 17:33:10 +0100

    ADD Cal_Distance
    Added a function that calculated the distance.

--- Script/Cal_Kinetic_Energy.m
+++ Script/Cal_Kinetic_Energy.m
@@ -1,6 +1,6 @@
 function Cal_Kinetic_Energy = func(Mass_counter, Length_counter, Mass_projectile, Length_projectile, Density, Start_angle, End_angle)
     %Calculats the avalible kinetic energy based on the potential gravitational energy: https://en.wikipedia.org/wiki/Gravitational_energy.
-    Comb = Length_counter*Mass_counter+0.5*Density*Length_counter^2-0.5*Density*Length_projectile^2-Length_projectile*Mass_projectile
+    Comb = Length_counter*Mass_counter+0.5*Density*Length_counter^2-0.5*Density*Length_projectile^2-Length_projectile*Mass_projectile;
     
     Cal_Kinetic_Energy = 9.8*(sin(End_angle)*Comb-sin(Start_angle)*Comb)
 end
