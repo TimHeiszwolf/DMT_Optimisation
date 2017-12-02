adcc2d072770ce956edb581dd82e45c70605b075
TimHeiszwolf <32781879+TimHeiszwolf@users.noreply.github.com>
2017-12-02 13:02:40 +0100

    IMPROV Cal_Distance
    Improved Cal_distance by reducing the variables that are assigned.

--- Script/Cal_Distance.m
+++ Script/Cal_Distance.m
@@ -1,4 +1,5 @@
 function Cal_Distance = func(Launch_velocity, Launch_angle, Start_Y, Length_counter, Length_projectile)
-    Air_time = (2*(sin(Launch_angle)*Launch_velocity+Start_Y)/9.8)^0.5;
-    Cal_Distance = cos(Launch_angle)*Launch_velocity*Air_time-cos(pi/2-Launch_angle)*(Length_counter+Length_projectile);
+    %Calculates the distance of the projectile.
+    %Air_time = ((2*(sin(Launch_angle)*Launch_velocity+Start_Y)/9.8)^0.5);
+    Cal_Distance = cos(Launch_angle)*Launch_velocity*(((sin(Launch_angle)*Launch_velocity+Start_Y)/4.9)^0.5)-cos(pi/2-Launch_angle)*(Length_counter+Length_projectile);
 end
