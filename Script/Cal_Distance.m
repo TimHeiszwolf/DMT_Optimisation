dd18a674f85eef7b7ddb4cd0c4aebce3c96f893a
TimHeiszwolf <32781879+TimHeiszwolf@users.noreply.github.com>
2017-12-04 16:47:06 +0100

    FIX Cal_Distance&Cal_Velocity no complex numbers+
    Fixed Cal_Distance and Cal_Velocity such that they don't return complex numbers

--- Script/Cal_Distance.m
+++ Script/Cal_Distance.m
@@ -1,5 +1,10 @@
 function Cal_Distance = func(Launch_velocity, Launch_angle, Start_Y, Length_counter, Length_projectile)
     %Calculates the distance of the projectile.
     %Air_time = ((2*(sin(Launch_angle)*Launch_velocity+Start_Y)/9.8)^0.5);
-    Cal_Distance = cos(Launch_angle)*Launch_velocity*(((sin(Launch_angle)*Launch_velocity+Start_Y)/4.9)^0.5)-cos(pi/2-Launch_angle)*(Length_counter+Length_projectile);
+    Distance = cos(Launch_angle)*Launch_velocity*(((sin(Launch_angle)*Launch_velocity+Start_Y)/4.9)^0.5)-cos(pi/2-Launch_angle)*(Length_counter+Length_projectile);
+    
+    if isreal(Distance)==1
+        Cal_Distance=Distance;
+    else
+        Cal_Distance=-10;
 end
--- Script/Cal_Velocity.m
+++ Script/Cal_Velocity.m
@@ -1,4 +1,9 @@
 function Cal_Velocity = func(Energy, Inertia, Length)
     %Function that calcultes the speed of a point on a rotation arm based on rotational energy https://en.wikipedia.org/wiki/Rotational_energy.
-    Cal_Velocity = Length*((2*Energy/Inertia)^0.5);
+    Velocity = Length*((2*Energy/Inertia)^0.5);
+    
+    if isreal(Velocity)==1
+        Cal_Velocity=Velocity;
+    else
+        Cal_Velocity=-10;
 end
