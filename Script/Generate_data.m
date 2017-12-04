395dc6b291c1b2eeacb7543681d3f8a2eaf4571e
TimHeiszwolf <32781879+TimHeiszwolf@users.noreply.github.com>
2017-12-04 15:09:58 +0100

    FIX Generate_data user input
    Fixed Generate_data's user input.

--- Script/Generate_data.m
+++ Script/Generate_data.m
@@ -1,16 +1,16 @@
 function Generate_data = func()
     %A function that generates the data for the controller
-    if (input("Manual start data entry (Y/N): ")=="Y")
-        Starting_height=input("Height of the launch platform (m): ");
-        Mass_counter=input("Mass of the counterweight (kg): ");
-        Mass_projectile=input("Mass of the projectile (kg): ");
-        Density_arms=input("Density of arms (kg/m): ");
-        Max_len_Aarm=input("Maximal length of projectile arm (m): ");
-        Max_steps_Aarm=input("Steps of projectile arm: ");
-        Max_len_Carm=input("Maximal length of counter-weight arm (m): ");
-        Max_steps_Carm=input("Steps of counter-weight arm: ");
-        Max_angle=input("Maximal launch angle (deg): ")*pi/180;
-        Max_stepts_angle=input("Steps of launch angle: ");
+    if (input('Manual start data entry (Y/N): ','s')=='Y')
+        Starting_height=input('Height of the launch platform (m): ');
+        Mass_counter=input('Mass of the counterweight (kg): ');
+        Mass_projectile=input('Mass of the projectile (kg): ');
+        Density_arms=input('Density of arms (kg/m): ');
+        Max_len_Aarm=input('Maximal length of projectile arm (m): ');
+        Max_steps_Aarm=input('Steps of projectile arm: ');
+        Max_len_Carm=input('Maximal length of counter-weight arm (m): ');
+        Max_steps_Carm=input('Steps of counter-weight arm: ');
+        Max_angle=input('Maximal launch angle (deg): ')*pi/180;
+        Max_stepts_angle=input('Steps of launch angle: ');
     else
         Starting_height=0.75;
         Mass_counter=0.224;
@@ -22,7 +22,9 @@ function Generate_data = func()
         Max_steps_Carm=4;
         Max_angle=60*pi/180;
         Max_stepts_angle=4;
+        disp('Assinged pre-defined values')
     end
-    
+    disp(' ')
+    disp('Starting combinatory generation')
     
 end
