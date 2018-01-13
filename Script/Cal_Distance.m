3c3cdb6309d157042bea1f4b2fc3b0059f4a2c09
TimHeiszwolf <32781879+TimHeiszwolf@users.noreply.github.com>
2018-01-13 16:43:06 +0100

    CHA Cal_Distance
    Changed Cal_Distance to use the ABC formula to calculate the airtime such that it is more clear that it is correct.

--- Script/Cal_Distance.m
+++ Script/Cal_Distance.m
@@ -1,6 +1,6 @@
 function Distance = Cal_Distance(Launch_velocity, Launch_angle, Start_Y, Length_counter, Length_projectile)
     %Calculates the distance the projectile will fly.
-    Airtime=((2*9.8*Start_Y+(Launch_velocity*sin(Launch_angle))^2)^0.5+Launch_velocity*sin(Launch_angle))/9.8;
+    Airtime=((Launch_velocity*sin(Launch_angle)+(2*9.8*Start_Y+(Launch_velocity*sin(Launch_angle))^2)^0.5))/9.8;
     Calculated_Distance = cos(Launch_angle)*Launch_velocity*Airtime-cos(pi/2-Launch_angle)*(Length_counter+Length_projectile);
     
     if isreal(Calculated_Distance)==1
